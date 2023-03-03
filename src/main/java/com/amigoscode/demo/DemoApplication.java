6da8f66755e3e4af2e02e1b225f47bc02e353e23
srb0830 <srikanthreddy.b0830@gmail.com>
2023-03-02 22:26:00 -0500

    Test Message log updated again

--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("test message updated");
+        System.out.println("test message updated again");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
