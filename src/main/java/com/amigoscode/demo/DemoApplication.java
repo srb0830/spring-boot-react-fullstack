e2638a1348819a89786f48cbb10b4bfe09d38c81
Srikanth Reddy Beerelly <srikanth@Srikanths-MacBook-Pro.local>
2025-01-21 16:56:26 -0500

    test commit

--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("test message updated again");
+        System.out.println("test message updated again and again");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
